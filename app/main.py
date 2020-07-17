bd4225644d46637cf9064717238d61408eedade8
JAPLJ <ys.qicos@gmail.com>
2020-07-17 22:55:08 +0900

    2 kai

--- app/main.py
+++ app/main.py
@@ -7,7 +7,15 @@ def main():
     player_key = sys.argv[2]
     print('ServerUrl: %s; PlayerKey: %s' % (server_url, player_key))
 
-    res = requests.post(server_url, data='010')
+    res = requests.post(server_url, data=player_key)
+    if res.status_code != 200:
+        print('Unexpected server response:')
+        print('HTTP code:', res.status_code)
+        print('Response body:', res.text)
+        exit(2)
+    print('Server response:', res.text)
+
+    res = requests.post(server_url, data='Hello!')
     if res.status_code != 200:
         print('Unexpected server response:')
         print('HTTP code:', res.status_code)
