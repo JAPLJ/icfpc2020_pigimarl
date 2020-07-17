713ec86cb9d2dba4db524b6e5ea375c047db7ae8
sugimori_ken <ken_sugimori@r.recruit.co.jp>
2020-07-18 06:51:37 +0900

    multiple draw の機能を取り込んだ

--- app/client.py
+++ app/client.py
@@ -1,5 +1,7 @@
 import requests
 
+from app.multiple_draw import multipul_draw
+
 
 def modulate(a):
     """
@@ -10,7 +12,24 @@ def modulate(a):
     :param a: modulatable な何か
     :return: 01 文字列
     """
-    return '01'
+    if a is None:
+        return '00'
+    elif type(a) is int:
+        res = []
+        if a >= 0:
+            res.append('01')
+        else:
+            res.append('10')
+        bits = '' if a == 0 else bin(abs(a))[2:]
+        bits4 = (len(bits) + 3) // 4
+        res.append('1' * bits4 + '0')
+        res.append('0' * (bits4 * 4 - len(bits)) + bits)
+        return ''.join(res)
+    elif (type(a) is tuple) and (len(a) == 2):
+        return '11' + modulate(a[0]) + modulate(a[1])
+    else:
+        print('Error! Not a modulatable object:', a)
+        exit(2)
 
 
 def demodulate(s):
@@ -22,7 +41,25 @@ def demodulate(s):
     :param s: 01 文字列
     :return: modulatable な何か
     """
-    return [0]
+
+    def dem_inner(s, k):
+        if s[k:k + 2] == '11':  # cons
+            (h, k) = dem_inner(s, k + 2)
+            (t, k) = dem_inner(s, k)
+            return (h, t), k
+        elif s[k:k + 2] == '00':  # nil
+            return None, k + 2
+        else:
+            sgn = +1 if s[k:k + 2] == '01' else -1
+            bits = 0
+            k = k + 2
+            while s[k] == '1':
+                bits += 1
+                k += 1
+            num = 0 if bits == 0 else sgn * int(s[k + 1:k + bits * 4 + 1], 2)
+            return num, k + bits * 4 + 1
+
+    return dem_inner(s, 0)[0]
 
 
 def interact(server_url, pictures_path, initial_data, initial_res):
@@ -47,7 +84,7 @@ def interact(server_url, pictures_path, initial_data, initial_res):
 
         if continue_flag == 0:
             print('[Draw] Pictures:', res)
-            multipledraw(pictures_path, res)
+            draw_pictures(pictures_path, res)
             break
 
         print('[Send] Request:', req)
@@ -79,22 +116,41 @@ def send(server_url, req):
     return res
 
 
-def galaxy(input_list):
+def galaxy(data, res):
     """
-    (データ, レスポンス) を受け取り、次の (継続フラグ, データ, リクエスト) を返す。
+    データとレスポンスを受け取り、次の (継続フラグ, データ, リクエスト) を返す。
     japlj さんが書いてくれてる。
-    :param input_list: 長さ 2 のリスト (データ, レスポンス)
-    :return: 長さ 3 の リスト (停止フラグ, データ, リクエスト)
+    :param data: データ
+    :param res: レスポンス
+    :return: 長さ 3 の リスト (継続フラグ, データ, リクエスト)
     """
     pass
 
 
-def multipledraw(path, pictures):
+def draw_pictures(pictures_path, pictures_cons_list):
     """
-    指定のパスに画像を出力する。
-    鈴木君が書いてくれてる。
-    :param path: 画像の出力先
-    :param pictures: List[List[Pair[Int, Int]]] で表現される画像たちのリスト
-    :return: None
+    指定のパスに画像たちを出力する。
+    :param pictures_path: 画像の出力先のパス
+    :param pictures_cons_list: 画像たちの cons 形式のリスト
+    :return:
     """
-    pass
+    pictures = cons_list_to_python_list(pictures_cons_list)
+    plot_vectors_list = [cons_list_to_python_list(picture) for picture in pictures]
+    multipul_draw(plot_vectors_list, output_dir=pictures_path)
+
+
+def cons_list_to_python_list(cons_list):
+    """
+    cons 形式のリストを Python 形式の (普通の) リストへ変換する。
+    :param cons_list: cons 形式のリスト
+    :return: Python 形式のリスト
+    """
+    if cons_list is None:
+        return []
+    elif (type(cons_list) is tuple) and (len(cons_list) == 2):
+        head = cons_list[0]
+        tail = cons_list[1]
+        return [head] + cons_list_to_python_list(tail)
+    else:
+        print('Error! Not a cons list:', cons_list)
+        exit(2)
