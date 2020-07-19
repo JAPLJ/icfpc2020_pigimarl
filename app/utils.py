4bce644086a8cbce6857aa7705eba7d358e576e1
sugimori_ken <ken_sugimori@r.recruit.co.jp>
2020-07-20 00:40:02 +0900

    calc_gravity 関数を追加

--- app/utils.py
+++ app/utils.py
@@ -16,6 +16,7 @@ def laser_damage(atk_ship_x, atk_ship_y, target_x, target_y, laser_power):
     v = max(0, v0 - (vmax * 2 * y // d))
     return abs(v)
 
+
 def next_pos(planet_r, gravity_r, x, y, vx, vy):
     if abs(x) <= abs(y):
         if y > 0:
@@ -29,3 +30,32 @@ def next_pos(planet_r, gravity_r, x, y, vx, vy):
             vx += 1
     x, y = x + vx, y + vy
     return (x, y, vx, vy)
+
+
+def sign(val):
+    if val > 0:
+        return 1
+    elif val < 0:
+        return -1
+    else:
+        return 0
+
+
+def calc_gravity(gravity_r, x, y):
+    """
+    座標 (x, y) に働く重力のベクトルを返す
+    :param gravity_r:
+    :param x:
+    :param y:
+    :return:
+    """
+    # 重力半径外
+    if max(abs(x), abs(y)) > gravity_r:
+        return 0, 0
+
+    if abs(x) > abs(y):
+        return -sign(x), 0
+    elif abs(x) < abs(y):
+        return 0, -sign(y)
+    else:
+        return -sign(x), -sign(y)
