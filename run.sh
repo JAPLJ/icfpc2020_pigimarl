837d051a70a3b0f10f39a42cb8458896a973ee7b
wakatsuki-ryohei <wakatsuki@r.recruit.co.jp>
2020-07-19 21:04:19 +0900

    runsh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-python app/main.py "$@" || echo "run error code: $?"
+# python app/main.py "$@" || echo "run error code: $?"
+python app/naive_orbit/naive_orbit.py "$@" || echo "run error code: $?"
