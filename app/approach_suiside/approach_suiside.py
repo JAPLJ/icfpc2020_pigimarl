2a4012f8d9ff2960ab2092b76ee63015808db1b7
keisho-oh <oh.keisho@gmail.com>
2020-07-19 22:49:20 +0900

    add model

---
+++ app/approach_suiside/approach_suiside.py
@@ -0,0 +1,75 @@
+import sys
+from math import sqrt
+from typing import Dict, Tuple, List
+
+import numpy as np
+
+sys.path.append('tools/')
+sys.path.append('app/')
+from utils import l_inf_dist, normalize_vector, calc_gravity_acceration, calc_normalized_direction
+from tournament_client import run
+from common_interface import GameStage, ShipParameter, Side
+from ai_interface import Ship, State
+
+
+def create_accel_command(x: int, y: int) -> Dict:
+    return {'command': 'accel', 'x': x, 'y': y}
+
+
+def create_laser_command(x: int, y: int, power: int) -> Dict:
+    return {'command': 'laser', 'x': x, 'y': y, 'power': power}
+
+
+def create_suiside_command() -> Dict:
+    return {'command': 'suiside'}
+
+
+def calc_next_pos(x, vx, y, vy) -> Tuple[int, int]:
+    return x + vx, y + vy
+
+
+class APPROACH_SUISIDE:
+    def __init__(self, cool=16):
+        self.suiside_dist_th = 5
+        self.cool = cool
+
+    def set_specs(self, limit: int, side: int) -> ShipParameter:
+        energy = limit - 12*self.cool - 2
+        return ShipParameter(energy, 0, self.cool, 1)
+
+    def action(self, state: State) -> Dict[int, List[Dict]]:
+
+        ret = {s.id: [] for s in state.my_ships}
+
+        # 分離しない場合を考える
+        s1 = state.my_ships[0]
+
+        enemy_dist = [l_inf_dist(s1.x, s1.y, s2.x, s2.y) for s2 in state.enemy_ships]
+        nearest_enemy_idx = np.argsort(enemy_dist)[0]
+        s2 = state.enemy_ships[nearest_enemy_idx]
+        p2_next = calc_next_pos(s2.x, s2.vx, s2.y, s2.vy)
+
+        if s1.params.energy > 2:
+            grav_acc = calc_gravity_acceration(s1.x, s1.y, state.gravity_radius)
+            enemy_dir = calc_normalized_direction(s1.x, s1.y, p2_next[0], p2_next[1])
+            a1 = normalize_vector( - grav_acc[0] + enemy_dir[0], - grav_acc[1] + enemy_dir[1])
+            p1_next = calc_next_pos(s1.x, a1[0], s1.y, a1[1])
+            ret[s1.id].append(create_accel_command(a1[0], a1[1]))
+        else:
+            p1_next = (s1.x, s1.y)
+
+        dist = l_inf_dist(p1_next[0], p1_next[1], p2_next[0], p2_next[1])
+
+        if dist < self.suiside_dist_th:
+            ret[s1.id].append(create_suiside_command())
+
+        return ret
+
+
+if __name__ == '__main__':
+    solver = APPROACH_SUISIDE()
+    server_url = sys.argv[1]
+    player_key = int(sys.argv[2])
+    json_log_path = None if len(sys.argv) < 4 else sys.argv[3]
+
+    run(server_url, player_key, solver, json_log_path=json_log_path)
--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,45 @@
+from typing import Tuple
+
+
+def l_inf_dist(x1, y1, x2, y2) -> int:
+    """
+    位置p1とp2のl_infty距離を計算する
+    """
+    return max(abs(x1 - x2), abs(y1 - y2))
+
+
+def normalize_vector(x, y) -> Tuple[int, int]:
+    if abs(x) > 0:
+        x //= abs(x)
+    if abs(y) > 0:
+        y //= abs(y)
+    return x, y
+
+
+def calc_normalized_direction(x1, y1, x2, y2) -> Tuple[int, int]:
+    """
+    (x1, y1) から (x2, y2) への向きを返す
+    ただし、各成分は -1, 0, 1のいずれかの値
+    """
+    dx = x2 - x1
+    dy = y2 - y1
+    return normalize_vector(dx, dy)
+
+
+def calc_gravity_acceration(x, y, gravity_radius) -> Tuple[int, int]:
+    d = l_inf_dist(x, y, 0, 0)
+    if d > gravity_radius:
+        return 0, 0
+
+    dir = calc_normalized_direction(x, y, 0, 0)
+    if abs(x) == abs(y) == d:
+        return dir
+    elif abs(x) == d:
+        return dir[0], 0
+    else:
+        return 0, dir[1]
+
+
 def laser_damage(atk_ship_x, atk_ship_y, target_x, target_y, laser_power):
     """
     (atk_ship_x, atk_ship_y) から(target_x, target_y)に対してレーザーを射出して、その地点へ与えるダメージ。
