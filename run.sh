45d6ef5a3783036e267e8dd99b5107f69519c5d2
keisho-oh <oh.keisho@gmail.com>
2020-07-19 23:36:03 +0900

    test

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 # python app/main.py "$@" || echo "run error code: $?"
-python app/naive_orbit/naive_orbit.py "$@" || echo "run error code: $?"
+# python app/naive_orbit/naive_orbit.py "$@" || echo "run error code: $?"
+python app/approach_suiside/approach_suiside.py "$@" || echo "run error code: $?"
