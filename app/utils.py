a2b01f2463cea08fa58ef153beac4a662ef30ce4
eto_mitsuhiro <mitsuhiro_eto@r.recruit.co.jp>
2020-07-19 19:03:22 +0900

    laser_damage func

---
+++ app/utils.py
@@ -0,0 +1,15 @@
+import math
+def laser_damage(atk_ship_x, atk_ship_y, target_x, target_y, laser_power):
+    """
+    (atk_ship_x, atk_ship_y) から(target_x, target_y)に対してレーザーを射出して、その地点へ与えるダメージ。
+    距離減衰は別に考える必要がある。
+    """
+    vmax = laser_power * 3
+    x_dist = abs(atk_ship_x - target_x)
+    y_dist = abs(atk_ship_y - target_y)
+    d = max([x_dist, y_dist])
+    v0 = vmax - (d - 1)
+    y = y_dist if x_dist > y_dist else x_dist
+    v = v0 - math.floor(vmax / d * 2 * y)
+    return abs(v)
+
