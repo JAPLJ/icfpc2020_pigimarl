c255d9d4c89b9f42e1ee8299b743f0af988b8188
JAPLJ <ys.qicos@gmail.com>
2020-07-19 19:20:31 +0900

    fix

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,3 @@
-import math
 def laser_damage(atk_ship_x, atk_ship_y, target_x, target_y, laser_power):
     """
     (atk_ship_x, atk_ship_y) から(target_x, target_y)に対してレーザーを射出して、その地点へ与えるダメージ。
@@ -8,8 +7,10 @@ def laser_damage(atk_ship_x, atk_ship_y, target_x, target_y, laser_power):
     x_dist = abs(atk_ship_x - target_x)
     y_dist = abs(atk_ship_y - target_y)
     d = max([x_dist, y_dist])
-    v0 = vmax - (d - 1)
+    v0 = max(0, vmax - (d - 1))
     y = y_dist if x_dist > y_dist else x_dist
-    v = v0 - math.floor(vmax / d * 2 * y)
+    if y > d // 2:
+        y = d - y
+    v = max(0, v0 - (vmax * 2 * y // d))
     return abs(v)
 
--- test/test_utils.py
+++ test/test_utils.py
@@ -3,22 +3,26 @@ sys.path.append('app')
 from utils import *
 
 # 8方向 => フルダメージ
-assert laser_damage(0, 0,  1,  0, 1), 3
-assert laser_damage(0, 0,  1,  0, 1), 3
-assert laser_damage(0, 0,  1,  1, 1), 3
-assert laser_damage(0, 0,  1, -1, 1), 3
-assert laser_damage(0, 0,  0,  1, 1), 3
-assert laser_damage(0, 0,  0, -1, 1), 3
-assert laser_damage(0, 0, -1,  0, 1), 3
-assert laser_damage(0, 0, -1,  1, 1), 3
-assert laser_damage(0, 0, -1, -1, 1), 3
+assert laser_damage(0, 0,  1,  0, 1) == 3
+assert laser_damage(0, 0,  1,  0, 1) == 3
+assert laser_damage(0, 0,  1,  1, 1) == 3
+assert laser_damage(0, 0,  1, -1, 1) == 3
+assert laser_damage(0, 0,  0,  1, 1) == 3
+assert laser_damage(0, 0,  0, -1, 1) == 3
+assert laser_damage(0, 0, -1,  0, 1) == 3
+assert laser_damage(0, 0, -1,  1, 1) == 3
+assert laser_damage(0, 0, -1, -1, 1) == 3
 
 # 最小ダメージ
-assert laser_damage(0, 0,  2,  1, 100), 1
-assert laser_damage(0, 0,  2, -1, 100), 1
-assert laser_damage(0, 0,  1,  2, 100), 1
-assert laser_damage(0, 0,  1, -2, 100), 1
-assert laser_damage(0, 0, -1,  2, 100), 1
-assert laser_damage(0, 0, -1, -2, 100), 1
-assert laser_damage(0, 0, -2,  1, 100), 1
-assert laser_damage(0, 0, -2, -1, 100), 1
+assert laser_damage(0, 0,  2,  1, 100) == 0
+assert laser_damage(0, 0,  2, -1, 100) == 0
+assert laser_damage(0, 0,  1,  2, 100) == 0
+assert laser_damage(0, 0,  1, -2, 100) == 0
+assert laser_damage(0, 0, -1,  2, 100) == 0
+assert laser_damage(0, 0, -1, -2, 100) == 0
+assert laser_damage(0, 0, -2,  1, 100) == 0
+assert laser_damage(0, 0, -2, -1, 100) == 0
+
+D = [227, 211, 195, 179, 163, 147, 131, 115, 98, 82, 66, 50, 34, 18, 2, 0, 0, 0, 2, 18, 34, 50, 66, 82, 98, 115, 131, 147, 163, 179, 195, 211, 227]
+for y in range(0, 33):
+    assert laser_damage(0, 0, 32, y, 86) == D[y]
