11034dfa4cd5728ced688d4c84a0a9f91bed684c
keisho-oh <oh.keisho@gmail.com>
2020-07-20 01:24:18 +0900

    test

--- app/approach_suiside/approach_suiside.py
+++ app/approach_suiside/approach_suiside.py
@@ -6,28 +6,31 @@ import numpy as np
 
 sys.path.append('tools/')
 sys.path.append('app/')
-from utils import l_inf_dist, normalize_vector, calc_gravity_acceration, calc_normalized_direction
+from utils import next_pos, go_into_orbit
 from tournament_client import run
 from common_interface import GameStage, ShipParameter, Side
 from ai_interface import Ship, State
 
 
-def create_accel_command(x: int, y: int) -> Dict:
-    return {'command': 'accel', 'x': x, 'y': y}
+def dist(x1, y1, x2, y2):
+    return max(abs(x1 - x2), abs(y1 - y2))
 
 
-def create_suiside_command() -> Dict:
-    return {'command': 'suiside'}
+def within_explosion(x1, y1, vx1, vy1, x2, y2, vx2, vy2, explotion_radius):
+    nx1, ny1, _, _ = next_pos(x1, y1, vx1, vy1)
+    nx2, ny2, _, _ = next_pos(x2, y2, vx2, vy2)
 
-
-def calc_next_pos(x, vx, y, vy) -> Tuple[int, int]:
-    return x + vx, y + vy
-
+    return dist(nx1, ny1, nx2, ny2) <= explotion_radius
+    
 
 class APPROACH_SUISIDE:
     def __init__(self, cool=16):
-        self.suiside_dist_th = 5
+        self.explosion_radius = 5
         self.cool = cool
+        self.stage = 1
+        self.into_orbit_moves = {}
+        self.into_orbit_idx = 0
+        self.into_orbit = False
 
     def set_specs(self, limit: int, side: int) -> ShipParameter:
         energy = limit - 12*self.cool - 2
@@ -40,24 +43,24 @@ class APPROACH_SUISIDE:
         # 分離しない場合を考える
         s1 = state.my_ships[0]
 
-        enemy_dist = [l_inf_dist(s1.x, s1.y, s2.x, s2.y) for s2 in state.enemy_ships]
+        enemy_dist = [max( abs(s1.x - s2.x), abs(s1.y - s2.y)) for s2 in state.enemy_ships]
         nearest_enemy_idx = np.argsort(enemy_dist)[0]
         s2 = state.enemy_ships[nearest_enemy_idx]
-        p2_next = calc_next_pos(s2.x, s2.vx, s2.y, s2.vy)
 
-        if s1.params.energy > 2:
-            grav_acc = calc_gravity_acceration(s1.x, s1.y, state.gravity_radius)
-            enemy_dir = calc_normalized_direction(s1.x, s1.y, p2_next[0], p2_next[1])
-            a1 = normalize_vector( - grav_acc[0] + enemy_dir[0], - grav_acc[1] + enemy_dir[1])
-            p1_next = calc_next_pos(s1.x, a1[0], s1.y, a1[1])
-            ret[s1.id].append(create_accel_command(a1[0], a1[1]))
-        else:
-            p1_next = (s1.x, s1.y)
-
-        dist = l_inf_dist(p1_next[0], p1_next[1], p2_next[0], p2_next[1])
-
-        if dist < self.suiside_dist_th and s1.side == Side.ATTACK:
-            ret[s1.id].append(create_suiside_command())
+        if self.stage == 1:
+            if not self.into_orbit and len(self.into_orbit_moves) == 0:
+                self.into_orbit_moves = go_into_orbit(state.planet_radius, s1.x, s1.y, s1.vx, s1.vy)
+            elif not self.into_orbit:
+                ms = self.into_orbit_moves[self.into_orbit_idx]
+                ret[s1.id].append({'command': 'accel', 'x': ms[0], 'y': ms[1]})
+                self.into_orbit_idx += 1
+                self.into_orbit = self.into_orbit_idx == len(self.into_orbit_moves)
+                if self.into_orbit:
+                    self.stage += 1
+        elif self.stage == 2:
+            suiside = within_explosion(s1.x, s1.y, s1.vx1, s1.vx2, s2.x, s2.y, s2.vx, s2.vy, self.explosion_radius)
+            if suiside:
+                ret[s1.id].append({'command': 'suiside'})
 
         return ret
 
