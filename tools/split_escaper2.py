9e420451daa7c934c8a3bb84088751ea81c201de
Hirosuke <hirosuke.sz@gmail.com>
2020-07-20 15:38:11 +0900

    imprve esc to atack

--- tools/split_escaper2.py
+++ tools/split_escaper2.py
@@ -12,7 +12,7 @@ MAX_TURNS = 256
 class SplitEscaper2:
     CHECK_TURN = 15  # 今後このターンで墜落・範囲外にならないことを確認
     ESCAPE_DMG = 50  # これ以上のダメージ（温度増加含む）で回避行動を取る
-    DUPLICATION_TURN = 8
+    DUPLICATION_TURN = 3
 
     def __init__(self):
         self.into_orbit_moves = None
@@ -20,7 +20,7 @@ class SplitEscaper2:
         self.mother_id = None
         self.duplication_count = self.DUPLICATION_TURN
 
-    def finc_valid_acc(self, ship, planet_radius, gravity_radius):
+    def finc_valid_acc(self, ship, planet_radius, gravity_radius, left_time):
         a_range = range(-ship.max_accel, ship.max_accel + 1)
         acc_candidate = []
         current_energy = utils.mechanical_energy(ship.x, ship.y, ship.vx, ship.vy)
@@ -37,7 +37,7 @@ class SplitEscaper2:
                 continue
             moves = [(ax, ay)]
             if utils.gravity_check(planet_radius, gravity_radius,
-                                   ship.x, ship.y, ship.vx, ship.vy, moves, self.CHECK_TURN):
+                                   ship.x, ship.y, ship.vx, ship.vy, moves, left_time):
                 acc_candidate.append((ax, ay))
         for acc in acc_candidate:
             if acc != self.past_acc:
@@ -45,6 +45,17 @@ class SplitEscaper2:
         if acc_candidate:
             return acc_candidate[0]
         return utils.stop(ship.x, ship.y, ship.vx, ship.vy)
+
+    def similar_energy_enemies(self, state, ship):
+        count = 0
+        current_m_energy = utils.mechanical_energy(ship.x, ship.y, ship.vx, ship.vy)
+        for enemy in state.enemy_ships:
+            if sum(enemy.params.list()) == 0:
+                continue
+            enemy_m_energy = utils.mechanical_energy(enemy.x, enemy.y, enemy.vx, enemy.vy)
+            if current_m_energy - 2 > enemy_m_energy or current_m_energy + 2 > enemy_m_energy:
+                count += 1
+        return count
 
     def action(self, state, ship):
         commands = []
@@ -59,13 +70,14 @@ class SplitEscaper2:
         if len(self.into_orbit_moves) > 0:
             acc = self.into_orbit_moves.pop(0)
         elif not utils.gravity_check(state.planet_radius, state.gravity_radius,
-                                     ship.x, ship.y, ship.vx, ship.vy, [], left_time):
-            acc = self.finc_valid_acc(ship, state.planet_radius, state.gravity_radius)
+                                     ship.x, ship.y, ship.vx, ship.vy, [], self.CHECK_TURN):
+            acc = self.finc_valid_acc(ship, state.planet_radius, state.gravity_radius, left_time)
 
         if acc != (0, 0):
             commands.append({'command': 'accel', 'x': acc[0], 'y': acc[1]})
 
-        if ship.params.soul > 2 and self.duplication_count < 0:
+        if ship.params.soul > 2 or self.duplication_count < 0:
+            acc = self.finc_valid_acc(ship, state.planet_radius, state.gravity_radius, left_time)
             self.duplication_count = self.DUPLICATION_TURN
             commands.append({'command': 'split', 'ship_ai_info': ShipAIInfo(SubShipAI(), 0, 0, 0, 1)})
 
