7b527ab0c5d90e16c28d8018084bf3d7e58586ef
sugimori_ken <ken_sugimori@r.recruit.co.jp>
2020-07-20 18:49:33 +0900

    ミサイルマン高速化

--- tools/missile_man.py
+++ tools/missile_man.py
@@ -72,14 +72,15 @@ class MissileMan:
 
             if len(self.go_into_orbit_accels) == 0 and self.turn % self.stalk_interval == 0:
                 self.go_into_orbit_accels = stalk(state.gravity_radius, state.planet_radius, ship.x, ship.y, ship.vx,
-                                                  ship.vy, 384 - self.turn, state.enemy_ships, self.stalk_max_energy)
+                                                  ship.vy, min(100, 384 - self.turn), state.enemy_ships,
+                                                  self.stalk_max_energy)
 
             if len(self.go_into_orbit_accels) > 0:
                 ax, ay = self.go_into_orbit_accels.pop(0)
                 commands.append({'command': 'accel', 'x': ax, 'y': ay})
             elif self.turn % self.missile_interval == 0:
                 accels = fire_target(state.gravity_radius, state.planet_radius, ship.x, ship.y, ship.vx, ship.vy,
-                                     384 - self.turn, state.enemy_ships, self.missile_max_energy, 1000)
+                                     min(100, 384 - self.turn), state.enemy_ships, self.missile_max_energy, 1000)
                 if accels is not None:
                     commands.append(
                         {'command': 'split', 'ship_ai_info': ShipAIInfo(Missile(accels), len(accels), 0, 0, 1)})
