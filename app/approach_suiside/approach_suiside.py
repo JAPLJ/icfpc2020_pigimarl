f11d0f0048e184338f3e6a3d3df45434e9d73261
keisho-oh <oh.keisho@gmail.com>
2020-07-20 14:09:58 +0900

    test multi ai

--- app/approach_suiside/approach_suiside.py
+++ app/approach_suiside/approach_suiside.py
@@ -7,10 +7,12 @@ import numpy as np
 
 sys.path.append('tools/')
 sys.path.append('app/')
-from utils import next_pos, go_into_orbit, DX, DY
+from utils import next_pos, go_into_orbit, DX, DY, move_to_target
 from tournament_client import run
 from common_interface import GameStage, ShipParameter, Side
 from ai_interface import Ship, State
+from multiship import ShipAI, ShipAIInfo, Multiship
+from ship_ai_example import MainShipAI
 
 
 def dist(x1, y1, x2, y2):
@@ -29,7 +31,20 @@ def random_direction():
     return DX[idx], DY[idx]
     
 
-class APPROACH_SUISIDE:
+class Suicider:
+    def __init__(self, count):
+        self.count = count  
+
+    def action(self, state, ship):
+        # カウントが 0 になったら自爆
+        if self.count == 0:
+            return [{'command': 'suicide'}]
+
+        self.count -= 1
+        return []
+
+
+class MainAI:
     def __init__(self, cool=16):
         self.explosion_radius = 5
         self.cool = cool
@@ -45,63 +60,47 @@ class APPROACH_SUISIDE:
         p4 =self.num_split
         return ShipParameter(limit - (p2*4 + p3*12 + p4*2), p2, p3, p4)
 
-    def action(self, state: State) -> Dict[int, List[Dict]]:
+    def action(self, state: State, ship: Ship) -> Dict[int, List[Dict]]:
 
-        ret = {s.id: [] for s in state.my_ships}
-
+        ret = []
         # 分離しない場合を考える
         if self.stage == 1:
-            s1 = state.my_ships[0]
-
-            enemy_dist = [max( abs(s1.x - s2.x), abs(s1.y - s2.y)) for s2 in state.enemy_ships]
-            nearest_enemy_idx = np.argsort(enemy_dist)[0]
-            s2 = state.enemy_ships[nearest_enemy_idx]
-
+            # enemy_dist = [max( abs(s1.x - s2.x), abs(s1.y - s2.y)) for s2 in state.enemy_ships]
+            # nearest_enemy_idx = np.argsort(enemy_dist)[0]
+            # s2 = state.enemy_ships[nearest_enemy_idx]
 
             if not self.into_orbit and len(self.into_orbit_moves) == 0:
-                self.into_orbit_moves = go_into_orbit(state.planet_radius, s1.x, s1.y, s1.vx, s1.vy)
+                self.into_orbit_moves = go_into_orbit(state.planet_radius, ship.x, ship.y, ship.vx, ship.vy)
             elif not self.into_orbit:
                 ms = self.into_orbit_moves[self.into_orbit_idx]
-                ret[s1.id].append({'command': 'accel', 'x': ms[0], 'y': ms[1]})
+                ret.append({'command': 'accel', 'x': ms[0], 'y': ms[1]})
                 self.into_orbit_idx += 1
                 self.into_orbit = self.into_orbit_idx == len(self.into_orbit_moves)
                 if self.into_orbit:
                     self.stage += 1
         elif self.stage == 2:
-            # print(f'[DEBUG] stage2 num my_ships = {len(state.my_ships)}')
-            if len(state.my_ships) < self.num_split:
-                for s1 in state.my_ships:
-                    if s1.params.soul >= 2:
-                        ret[s1.id].append({
-                            'command': 'split',
-                            'p1': s1.params.energy//2,
-                            'p2': s1.params.laser_power//2,
-                            'p3': s1.params.cooling_rate//2,
-                            'p4': s1.params.soul//2
-                        })
-                    # print(f'[DEBUG] len(ret[s1.id]) = {len(ret[s1.id])}')
-                if len(ret) >= self.num_split//2:
-                    self.stage += 1
-        elif self.stage == 3:
-            for s1 in state.my_ships:
-                dx, dy = random_direction()
-                ret[s1.id].append({'command': 'accel', 'x': dx, 'y': dy})
-            self.stage += 1
-        else:
-            for s1 in state.my_ships:
-                for s2 in state.enemy_ships:
-                    suiside = within_explosion(s1.x, s1.y, s1.vx, s1.vy, s2.x, s2.y, s2.vx, s2.vy, self.explosion_radius)
-                    if suiside:
-                        ret[s1.id].append({'command': 'suiside'})
-                    break
-
+            if ship.params.soul > 1:
+                ret.append({
+                    'command': 'split',
+                    'ship_ai_info': ShipAIInfo(Suicider(count=20), 0, 0, 0, 1)
+                })
+            else:
+                self.stage += 1
+        # elif self.stage == 3:
+        #     for s1 in state.my_ships:
+        #         dx, dy = random_direction()
+        #         ret.append({'command': 'accel', 'x': dx, 'y': dy})
+        #     self.stage += 1
+        
         return ret
 
 
 if __name__ == '__main__':
-    solver = APPROACH_SUISIDE()
+    attacker = ShipAIInfo(MainAI(), 152, 0, 10, 120)
+    defender = ShipAIInfo(MainShipAI(), 100, 0, 8, 100)
+
     server_url = sys.argv[1]
     player_key = int(sys.argv[2])
     json_log_path = None if len(sys.argv) < 4 else sys.argv[3]
 
-    run(server_url, player_key, solver, json_log_path=json_log_path)
+    run(server_url, player_key, Multiship(attacker, defender), json_log_path=json_log_path)
