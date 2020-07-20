5b8c558444b22feadf5bcf696154865612a99712
sugimori_ken <ken_sugimori@r.recruit.co.jp>
2020-07-20 17:01:52 +0900

    うごてくれ

--- tools/ai_selector.py
+++ tools/ai_selector.py
@@ -1,13 +1,14 @@
 from common_interface import *
+from missile_man import MissileMan
 from multiship import ShipAIInfo
-from ship_ai_example import MainShipAI
+
 
 # 君だけの AISelector を作って最高の AI を選択しよう！
 class AISelector:
     def select(self, side, limit, enemy_params) -> ShipAIInfo:
         if side == Side.ATTACK:
             # enemy_params is ShipParameter
-            return ShipAIInfo(MainShipAI(), 152, 0, 10, 120)
+            return ShipAIInfo(MissileMan(), 512 - (12 * 8 + 2 * 128), 0, 8, 128)
         else:
             # enemy_params is None
-            return ShipAIInfo(MainShipAI(), 100, 0, 8, 100)
+            return ShipAIInfo(MissileMan(), 448 - (12 * 8 + 2 * 128), 0, 8, 128)
