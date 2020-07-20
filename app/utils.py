bd959983a8b691ea37e087a889a7327fe6fc9ce5
sugimori_ken <ken_sugimori@r.recruit.co.jp>
2020-07-20 13:01:31 +0900

    MissileMan 相手と逆方向に回るようにした

--- app/utils.py
+++ app/utils.py
@@ -60,23 +60,27 @@ DX = [-1, -1, -1, 0, 1, 1, 1, 0]
 DY = [-1, 0, 1, 1, 1, 0, -1, -1]
 
 
-def gravity_check(planet_r, x0, y0, vx0, vy0, moves):
+def gravity_check(planet_r, x0, y0, vx0, vy0, moves, rot_sign=None):
     """
     (x0, y0) から速度 (vx0, vy0) で始めて、かつ最初の len(moves) 回は moves に従って accel するとする
     このとき 256 ターンにわたって墜落しないかつ外に出ないなら True を返す
     """
     x, y = x0, y0
     vx, vy = vx0, vy0
+    rot_sum = 0
     for i in range(256):
         if i < len(moves):
             vx, vy = vx + moves[i][0], vy + moves[i][1]
         (x, y, vx, vy) = next_pos(x, y, vx, vy)
+        rot_sum += x * vy - y * vx
         if max(abs(x), abs(y)) <= planet_r:
             return False
-    return True
+    if rot_sign is None:
+        return True
+    return sign(rot_sum) == rot_sign
 
 
-def go_into_orbit(planet_r, x0, y0, vx0, vy0):
+def go_into_orbit(planet_r, x0, y0, vx0, vy0, rot_sign=None):
     """
     (x0, y0) から速度 (vx0, vy0) で始めたとき、256 ターンにわたって墜落しないかつ外に出ないような
     最初に行うべき accel の列を返す
@@ -89,7 +93,7 @@ def go_into_orbit(planet_r, x0, y0, vx0, vy0):
             d = p[i]
             dx, dy = DX[d], DY[d]
             ms = [(dx, dy) for i in range(ln)]
-            if gravity_check(planet_r, x0, y0, vx0, vy0, ms):
+            if gravity_check(planet_r, x0, y0, vx0, vy0, ms, rot_sign):
                 return ms
         ln += 1
 
--- tools/missile_man.py
+++ tools/missile_man.py
@@ -33,22 +33,33 @@ class Missile:
 
 class MissileMan:
     def __init__(self):
-        self.go_into_orbit_accels = []
+        self.go_into_orbit_accels = None
         self.turn = 0
 
     def action(self, state, ship):
         commands = []
 
-        if self.turn % 20 == 0:
-            self.go_into_orbit_accels = go_into_orbit(state.planet_radius, ship.x, ship.y, ship.vx, ship.vy)
+        # 最初の数ターンは様子見
+        if self.turn < 10:
+            gx, gy = calc_gravity(ship.x, ship.y)
+            commands.append({'command': 'accel', 'x': -gx, 'y': -gy})
 
-        if len(self.go_into_orbit_accels) > 0:
-            ax, ay = self.go_into_orbit_accels.pop(0)
-            commands.append({'command': 'accel', 'x': ax, 'y': ay})
+        else:
+            if self.go_into_orbit_accels is None:
+                rot_sum = 0
+                for ship in state.enemy_ships:
+                    rot_sum += ship.x * ship.vy - ship.y * ship.vx
+                rot_sign = 1 if rot_sum > 0 else -1
+                self.go_into_orbit_accels = go_into_orbit(state.planet_radius, ship.x, ship.y, ship.vx, ship.vy,
+                                                          -rot_sign)
 
-        if self.turn % 2 == 0:
-            commands.append(
-                {'command': 'split', 'ship_ai_info': ShipAIInfo(Missile([random.choice(neighbours)]), 1, 0, 0, 1)})
+            if len(self.go_into_orbit_accels) > 0:
+                ax, ay = self.go_into_orbit_accels.pop(0)
+                commands.append({'command': 'accel', 'x': ax, 'y': ay})
+
+            if self.turn % 2 == 0:
+                commands.append(
+                    {'command': 'split', 'ship_ai_info': ShipAIInfo(Missile([random.choice(neighbours)]), 1, 0, 0, 1)})
 
         self.turn += 1
 
