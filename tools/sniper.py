75f589bcc866f2415146bd36f7dad9ffbabc7f11
wakatsuki-ryohei <wakatsuki@r.recruit.co.jp>
2020-07-20 17:03:33 +0900

    さきよみ

--- tools/sniper.py
+++ tools/sniper.py
@@ -12,30 +12,28 @@ class Sniper:
         self.into_orbit = dict()
         self.into_orbit_moves = dict()
         self.into_orbit_moves_idx = dict()
-        # self.eship_accel_history = defaultdict(list)
-        # self.turns = set()
+        self.eship_accel_history = defaultdict(list)
+        self.turns = set()
 
     def action(self, state, ship):
         res = []
 
         # ターンの最初の呼び出しだけ処理したい
-        # if state.current_turn not in self.turns:
-        #     self.turns.add(state.current_turn)
-        #     for eship in state.enemy_ships:
-        #         if sum(eship.params.list()) == 0:
-        #             # 死んでるし
-        #             continue
-        #         rc_accel = [rc for rc in eship.commands if rc.kind == 0]
-        #         if len(rc_accel) == 0:
-        #             self.eship_accel_history[eship.id].append((0, 0))
-        #         else:
-        #             self.eship_accel_history[eship.id].append((rc_accel[0].x, rc_accel[0].y))
+        if state.current_turn not in self.turns:
+            self.turns.add(state.current_turn)
+            for eship in state.enemy_ships:
+                if sum(eship.params.list()) == 0:
+                    # 死んでるし
+                    continue
+                rc_accel = [rc for rc in eship.commands if rc.kind == 0]
+                if len(rc_accel) == 0:
+                    self.eship_accel_history[eship.id].append((0, 0))
+                else:
+                    self.eship_accel_history[eship.id].append((rc_accel[0].x, rc_accel[0].y))
 
         if ship.id not in self.into_orbit:
             self.into_orbit[ship.id] = False
-            print('hoge', state.planet_radius, state.gravity_radius, ship.x, ship.y, ship.vx, ship.vy, file=sys.stderr)
             self.into_orbit_moves[ship.id] = go_into_orbit(state.gravity_radius, state.planet_radius, ship.x, ship.y, ship.vx, ship.vy)
-            print('fuga', file=sys.stderr)
             self.into_orbit_moves_idx[ship.id] = 0
 
         if not self.into_orbit[ship.id]:
@@ -53,13 +51,13 @@ class Sniper:
 
             pvx, pvy = 0, 0
             # # 次の一手が直近N手の周期性から予測できる場合はそれを使い、そうでないなら最新のaccelのものを使う
-            # next_command = guess_next(self.eship_accel_history[eship.id][-10:])
-            # if next_command is not None:
-            #     pvx, pvy = -next_command[0], -next_command[1]
-            # else:
-            for rc in eship.commands:
-                if rc.kind == 0:
-                    pvx, pvy = -rc.x, -rc.y
+            next_command = guess_next(self.eship_accel_history[eship.id][-10:])
+            if next_command is not None:
+                pvx, pvy = -next_command[0], -next_command[1]
+            else:
+                for rc in eship.commands:
+                    if rc.kind == 0:
+                        pvx, pvy = -rc.x, -rc.y
 
             (nx, ny, _, _) = next_pos(eship.x, eship.y, eship.vx + pvx, eship.vy + pvy)
             max_lp = min(ship.params.laser_power, ship.max_temp - ship.temp)
