b54bbbb9ec491c8846252d33d0ff07baf66a2111
sugimori_ken <ken_sugimori@r.recruit.co.jp>
2020-07-20 10:29:34 +0900

    count を増やしてみた

--- tools/ship_ai_example.py
+++ tools/ship_ai_example.py
@@ -18,7 +18,7 @@ class MainShipAI:
 
         # soul がある間は SubShipAI を産み続ける
         if ship.params.soul > 1:
-            commands.append({'command': 'split', 'ship_ai_info': ShipAIInfo(SubShipAI(count=5), 0, 0, 0, 1)})
+            commands.append({'command': 'split', 'ship_ai_info': ShipAIInfo(SubShipAI(count=20), 0, 0, 0, 1)})
 
         return commands
 
